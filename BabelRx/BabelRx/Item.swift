3f6e159de37f85a3be7650096e92d648d76b700f
kevnster <kevxngyn@gmail.com>
2025-05-06 00:11:45 -0400

    Init commit: Creating XCode project structure

---
+++ BabelRx/BabelRx/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BabelRx
+//
+//  Created by Kevin Nguyen on 5/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
